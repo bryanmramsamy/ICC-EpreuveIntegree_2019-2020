1f21f480fcf106a76980ff3ba35e56376c5f1c22
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-07-15 22:32:06 +0200

    Code lint on customLogout view

--- serina-project/registration/views.py
+++ serina-project/registration/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from django.utils.translation import ugettext as _
 
 from .forms import CustomAuthenticationForm
 
@@ -13,21 +14,10 @@ class CustomLoginView(LoginView):
     template_name = "registration/login.html"
     authentication_form = CustomAuthenticationForm
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        if self.request.GET.get('logout') is not None:
-            messages.success(
-                self.request,
-                "You have been logged out successfully"
-            )
-
-        return context
-
 
 def customLogout(request):
     """Logout redirection"""
 
     logout(request)
-    messages.success(request, "You have been logged out successfully")
-    return redirect('{}?logout=True'.format(reverse('login')))
+    messages.success(request, _("You have been logged out successfully"))
+    return redirect(reverse('login'))
