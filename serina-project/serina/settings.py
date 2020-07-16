caa842d331e672cc6d3af92f9ff86fc0d7cbc42e
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-07-16 19:45:50 +0200

    Add SMTP settings

--- serina-project/serina/settings.py
+++ serina-project/serina/settings.py
@@ -146,3 +146,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# SMTP configuration
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_USE_TLS = True
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
