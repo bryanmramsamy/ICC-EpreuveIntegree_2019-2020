2b9189895c50a67c041ad637a99b5e6e0a04c7de
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-07-24 20:44:36 +0200

    Hotfix: Make related_name arguments on ManyToManyFields more self-explanatory

--- serina-project/management/models/module.py
+++ serina-project/management/models/module.py
@@ -44,13 +44,22 @@ class Module(Resource):
     title = models.CharField(max_length=255, verbose_name=_("Title"))
     description = models.TextField(null=True, blank=True,
                                    verbose_name=_("Description"))
-    student = models.ManyToManyField(User, related_name='students')
-    teachers = models.ManyToManyField(User, related_name='teachers')
-    is_prerequisite_for = models.ForeignKey('self', on_delete=models.SET_NULL,
-                                            null=True, blank=True,
-                                            verbose_name=_(
-                                                "Is a prerequisite for"
-                                            ))
+    students = models.ManyToManyField(
+        User,
+        related_name='modules_followed_as_student',
+        verbose_name=_('Modules followed as a student')
+    )
+    teachers = models.ManyToManyField(
+        User,
+        related_name='modules_teached_as_teacher',
+        verbose_name=_('Modules teached as a student')
+    )
+    is_prerequisite_for = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True, blank=True,
+        verbose_name=_("Is a prerequisite for")
+    )
     level = models.ForeignKey(ModuleLevel, on_delete=models.SET_NULL,
                               null=True, verbose_name=_('Difficulty level'))
     date_start = models.DateField(verbose_name=_('Start date'))
