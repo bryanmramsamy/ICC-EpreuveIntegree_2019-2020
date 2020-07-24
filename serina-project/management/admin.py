416242e4b0b39eea51846cd125ba7033bad407b7
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-07-24 20:15:21 +0200

    Add ModuleLevel and Module models to admin (basic)

--- serina-project/management/admin.py
+++ serina-project/management/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.utils.translation import ugettext as _
 
-# Register your models here.
+from .models import Module, ModuleLevel
+
+
+class ModuleAdmin(admin.ModelAdmin):
+    """UserProfile admin registration class."""
+
+    model = Module
+
+
+class ModuleLevelAdmin(admin.ModelAdmin):
+    """UserProfile admin registration class."""
+
+    model = ModuleLevel
+
+
+admin.site.register(Module, ModuleAdmin)
+admin.site.register(ModuleLevel, ModuleLevelAdmin)
