0dc3d95dfff5a8cbd37aed758751488ec4ecae36
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-07-24 19:58:02 +0200

    Add ModuleLevel model and Modules fields

--- serina-project/management/models/module.py
+++ serina-project/management/models/module.py
@@ -2,26 +2,61 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import ugettext as _
 
-from .element import Element
+from .ressource import Ressource
 
 
-class Module(Element):
-    """Model definition for Module."""
+class ModuleLevel(models.Model):
+    """Model definition for ModuleLevel.
+
+    A ranked difficulty level assign to each module.
+    """
+
+    rank = models.IntegerField(unique=True, verbose_name=_('Rank'))
+    name = models.CharField(max_length=50, verbose_name=_('Name'))
+
+    class Meta:
+        """Meta definition for ModuleLevel."""
+
+        verbose_name = 'Modules level'
+        verbose_name_plural = 'Modules levels'
+        ordering = ('rank',)
+
+    def __str__(self):
+        """Unicode representation of ModuleLevel."""
+
+        return "({}) {}".format(self.rank, self.name)
+
+    # TODO: Define method when rooters initialized
+    # def get_absolute_url(self):
+    #     """Return absolute url for ModuleLevel."""
+    #     return ('')
+
+
+class Module(Ressource):
+    """Model definition for Module.
+
+    A module is a course.
+    This one can be a prerequisite for other modules, which means these
+    modules cannot be started as long as this one isn't finisehd yet.
+    Each module has can be teached by specific teachers.
+    """
 
     title = models.CharField(max_length=255, verbose_name=_("Title"))
     description = models.TextField(null=True, blank=True,
                                    verbose_name=_("Description"))
+    student = models.ManyToManyField(User, related_name='students')
+    teachers = models.ManyToManyField(User, related_name='teachers')
     is_prerequisite_for = models.ForeignKey('self', on_delete=models.SET_NULL,
                                             null=True, blank=True,
                                             verbose_name=_(
                                                 "Is a prerequisite for"
                                             ))
-    # level = models.ForeignKey(ModuleLevel, on_delete=models.SET_NULL,
-    #                           null=True, verbose_name=_('Difficulty level'))
-    # TODO: Define ModuleLevel model first
+    level = models.ForeignKey(ModuleLevel, on_delete=models.SET_NULL,
+                              null=True, verbose_name=_('Difficulty level'))
+    date_start = models.DateField(verbose_name=_('Start date'))
+    date_end = models.DateField(verbose_name=_('End date'))
     cost = models.FloatField(verbose_name=_('Cost'))
     fee = models.FloatField(verbose_name=_('Registration fee'))
-    nbPeriods = models.PositiveIntegerField(verbose_name=_('Periods'))
 
     class Meta:
         """Meta definition for Module."""
