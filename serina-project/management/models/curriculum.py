13ac6b93f388201b64da9cdc84963133a0d55cd4
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-07-24 20:28:36 +0200

    Add CurriculumCategory model

--- serina-project/management/models/curriculum.py
+++ serina-project/management/models/curriculum.py
@@ -1,2 +1,30 @@
 from django.db import models
 from django.utils.translation import ugettext as _
+
+from .resource import Resource
+
+
+class CurriculumCategory(Resource):
+    """Model definition for CurriculumCategory.
+
+    A category assign to each curriculum.
+    """
+
+    name = models.CharField(max_length=50,
+                            verbose_name=_('Curriculum category'))
+
+    class Meta:
+        """Meta definition for CurriculumCategory."""
+
+        verbose_name = 'Curriculum category'
+        verbose_name_plural = 'Curriculum categories'
+
+    def __str__(self):
+        """Unicode representation of CurriculumCategory."""
+
+        return "[{}] {}".format(self.pk, self.name)
+
+    # # TODO: Define method when rooters initialized
+    # def get_absolute_url(self):
+    #     """Return absolute url for CurriculumCategory."""
+    #     return ('')
