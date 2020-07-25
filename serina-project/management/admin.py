2cd57d53c0f7cb55df1fcfa69920e3269ea7e6f4
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-07-25 22:42:41 +0200

    Register Degree and DegreeCategory in admin (basic)

--- serina-project/management/admin.py
+++ serina-project/management/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.utils.translation import ugettext as _
 
-from .models import Module, ModuleLevel
+from .models import Degree, DegreeCategory, Module, ModuleLevel
 
 
+admin.site.register(Degree)
+admin.site.register(DegreeCategory)
 admin.site.register(Module)
 admin.site.register(ModuleLevel)
