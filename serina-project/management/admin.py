2c546a9d752d6d613187f2a43fa0309a2f7a8e6b
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-07-25 20:42:04 +0200

    Register Module and ModuleLevel in admin (basic)

--- serina-project/management/admin.py
+++ serina-project/management/admin.py
@@ -1,2 +1,8 @@
 from django.contrib import admin
 from django.utils.translation import ugettext as _
+
+from .models import Module, ModuleLevel
+
+
+admin.site.register(Module)
+admin.site.register(ModuleLevel)
