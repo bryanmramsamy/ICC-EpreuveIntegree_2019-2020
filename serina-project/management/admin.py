3edd3c539ab94e6f71208ff135948afa5c9befb6
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-07-25 18:32:39 +0200

    Revert all management-app models from admin

--- serina-project/management/admin.py
+++ serina-project/management/admin.py
@@ -4,31 +4,31 @@ from django.utils.translation import ugettext as _
 from .models import Curriculum, CurriculumCategory, Module, ModuleLevel
 
 
-class CurriculumAdmin(admin.ModelAdmin):
-    """Curriculum admin registration class."""
+# class CurriculumAdmin(admin.ModelAdmin):
+#     """Curriculum admin registration class."""
 
-    model = Curriculum
-
-
-class CurriculumCategoryAdmin(admin.ModelAdmin):
-    """CurriculumCategory admin registration class."""
-
-    model = CurriculumCategory
+#     model = Curriculum
 
 
-class ModuleAdmin(admin.ModelAdmin):
-    """ModuleAdmin admin registration class."""
+# class CurriculumCategoryAdmin(admin.ModelAdmin):
+#     """CurriculumCategory admin registration class."""
 
-    model = Module
-
-
-class ModuleLevelAdmin(admin.ModelAdmin):
-    """ModuleLevelAdmin admin registration class."""
-
-    model = ModuleLevel
+#     model = CurriculumCategory
 
 
-admin.site.register(Curriculum, CurriculumAdmin)
-admin.site.register(CurriculumCategory, CurriculumCategoryAdmin)
-admin.site.register(Module, ModuleAdmin)
-admin.site.register(ModuleLevel, ModuleLevelAdmin)
+# class ModuleAdmin(admin.ModelAdmin):
+#     """ModuleAdmin admin registration class."""
+
+#     model = Module
+
+
+# class ModuleLevelAdmin(admin.ModelAdmin):
+#     """ModuleLevelAdmin admin registration class."""
+
+#     model = ModuleLevel
+
+
+# admin.site.register(Curriculum, CurriculumAdmin)
+# admin.site.register(CurriculumCategory, CurriculumCategoryAdmin)
+# admin.site.register(Module, ModuleAdmin)
+# admin.site.register(ModuleLevel, ModuleLevelAdmin)
