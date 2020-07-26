049f3dc0ebcd4f659b1f3858caee0c73bc330fa0
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-07-26 03:27:13 +0200

    Register Classroom and Course models in admin (basic)

--- serina-project/management/admin.py
+++ serina-project/management/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from django.utils.translation import ugettext as _
 
-from .models import Degree, DegreeCategory, Module, ModuleLevel
+from .models import (
+    Classroom,
+    Course,
+    Degree,
+    DegreeCategory,
+    Module,
+    ModuleLevel
+)
 
 
+admin.site.register(Classroom)
+admin.site.register(Course)
 admin.site.register(Degree)
 admin.site.register(DegreeCategory)
 admin.site.register(Module)
