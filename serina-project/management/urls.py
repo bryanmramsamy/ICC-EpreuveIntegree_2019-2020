696f9f5b2d58ddf0cf8ac1a1013bde038c5df5ae
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-07-28 21:37:59 +0200

    Debug: Add DegreeListView, DegreeCategoryDetailView roots, views and templates

--- serina-project/management/urls.py
+++ serina-project/management/urls.py
@@ -6,6 +6,19 @@ from . import views
 urlpatterns = [
     url(r"^$", views.home, name="home"),
 
+    url(r"^degree/list/$", views.DegreeListView.as_view(),
+        name="degree_listview"),
+    url(r"^degree/read/(?P<pk>[0-9]+)/$", views.DegreeDetailView.as_view(),
+        name="degree_detailview"),
+
+    url(r"^degree/category/list/$", views.DegreeCategoryListView.as_view(),
+        name="degreecategory_listview"),
+    url(
+        r"^degree/category/read/(?P<pk>[0-9]+)/$",
+        views.DegreeCategoryDetailView.as_view(),
+        name="degreecategory_detailview"
+    ),
+
     url(r"^module/list/$", views.ModuleListView.as_view(),
         name="module_listview"),
     url(r"^module/read/(?P<pk>[0-9]+)/$", views.ModuleDetailView.as_view(),
@@ -18,9 +31,4 @@ urlpatterns = [
         views.ModuleLevelDetailView.as_view(),
         name="modulelevel_detailview"
     ),
-
-    url(r"^degree/list/$", views.DegreeListView.as_view(),
-        name="degree_listview"),
-    url(r"^degree/read/(?P<pk>[0-9]+)/$", views.DegreeDetailView.as_view(),
-        name="degree_detailview"),
 ]
--- serina-project/management/views/degree.py
+++ serina-project/management/views/degree.py
@@ -19,3 +19,20 @@ class DegreeDetailView(DetailView):  # TODO: Debug view
     model = Degree
     template_name = "management/degree_detailview.html"
     context_object_name = "degree"
+
+
+class DegreeCategoryListView(ListView):  # TODO: Debug view
+    """ListView for DegreeCategory"""
+
+    model = DegreeCategory
+    template_name = "management/degreecategory_listview.html"
+    context_object_name = "categories"
+    paginate_by = 10
+
+
+class DegreeCategoryDetailView(DetailView):  # TODO: Debug view
+    """DetailView for DegreeCategory"""
+
+    model = DegreeCategory
+    template_name = "management/degreecategory_detailview.html"
+    context_object_name = "category"
