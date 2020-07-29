b404d3ab040a28314d0f9023ffd93172759c527d
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-07-29 23:18:30 +0200

    Create FrontOfficeResource model

--- serina-project/inscription/models.py
+++ serina-project/inscription/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.shortcuts import reverse
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+
+class FrontOfficeResource(models.Model):
+    """Model definition for FrontOfficeResource.
+
+    A ressource contains a creation and last update timestamp.
+    The FrontOfficeResource model is inherited by each front-office model.
+    """
+
+    date_created = models.DateTimeField(auto_now_add=True,
+                                        verbose_name=_('Created on'))
+    date_updated = models.DateTimeField(auto_now=True,
+                                        verbose_name=_('Updated on'))
+
+    class Meta:
+        """Meta definition for FrontOfficeResource."""
+
+        abstract = True
