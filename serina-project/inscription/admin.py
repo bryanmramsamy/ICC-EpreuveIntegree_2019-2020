bcad19b6b8299bccc31942f7cbc3dd67b6a99fa0
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-07-30 20:51:50 +0200

    Register models to admin (basic)

--- serina-project/inscription/admin.py
+++ serina-project/inscription/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    DegreeRegistrationReport,
+    ModuleRegistrationReport,
+    StudentRegistrationReport,
+)
+
+
+admin.site.register(DegreeRegistrationReport)
+admin.site.register(ModuleRegistrationReport)
+admin.site.register(StudentRegistrationReport)
