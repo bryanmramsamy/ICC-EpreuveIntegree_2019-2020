08ce7665f37e9a498e8ba7636e4623c3d4a57791
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-08-02 14:58:29 +0200

    Add Custom ModuleMultipleChoiceField __str__ and queryset

--- serina-project/management/forms/module.py
+++ serina-project/management/forms/module.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.db.models import Q
 
 from ..models import Module, ModuleLevel
 from .resource import BackOfficeResourceFormMixin
@@ -13,9 +14,24 @@ class ModuleCreateForm(BackOfficeResourceFormMixin):
         model = Module
         exclude = ("reference", "prerequisites", "eligible_teachers")
 
+class MyModelMultipleChoiceField(forms.ModelMultipleChoiceField):
+    def label_from_instance(self, obj):
+        return "{}".format(obj.title)
 
 class ModuleUpdateForm(BackOfficeResourceFormMixin):
-    """ModelForm for Module."""
+    """ModelForm for Module.
+
+    Prevent the user to add the instance to its own prerequisites. Also prevent
+    adding a postrequisite module to the prerequisites."""
+
+    prerequisites = MyModelMultipleChoiceField(queryset=None)
+
+    def __init__(self, *args, **kwargs):
+        """Init of the 'prerequisites'-field queryset."""
+
+        super().__init__(*args, **kwargs)
+        self.fields['prerequisites'].queryset = Module.objects.exclude(
+            pk=self.instance.pk).exclude(prerequisites=self.instance)
 
     class Meta(BackOfficeResourceFormMixin.Meta):
         """Meta definition for ModuleLevelForm."""
