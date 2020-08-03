6815795b2642d3e2c53f1c03cc50329b7384fa8e
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-08-03 20:00:41 +0200

    Optimized runserver_plus.sh launch script

--- serina-project/scripts/runserver_plus.sh
+++ serina-project/scripts/runserver_plus.sh
@@ -5,11 +5,9 @@
 
 while true; do
     if ! python3 manage.py runserver_plus 0.0.0.0:8000; then
-        printf "---------------------------------------------\n RUNSERVER_PLUS EXITED WITH A NON-0 STATUS ! \n      >>> Restarting in 3 seconds <<<      \n---------------------------------------------\n"
+        printf "_________________________________________________\n>>> RUNSERVER_PLUS EXITED WITH A NON-0 STATUS <<<\n>>>          Restarting in 3 seconds          <<<\n"
         sleep 3;
     else
         break
     fi
 done
-
-printf "----------------------------------\n RUNSERVER_PLUS GRACEFULLY EXITED \n----------------------------------\n"
