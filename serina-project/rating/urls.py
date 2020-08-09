16d8b68abd16ae546a4498bc013874a657951836
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-08-09 20:24:29 +0200

    Add StudentRating Create view, url and template

--- serina-project/rating/urls.py
+++ serina-project/rating/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
 
+from . import views
+
 
 urlpatterns = [
-    
+    url(
+        r"^c/$",
+        views.StudentRatingCreateView.as_view(),
+        name="rating_createview"
+    )
 ]
--- serina-project/rating/views.py
+++ serina-project/rating/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import StudentRatingForm
+from .models import StudentRating
+from registration.utils import mixins as mixins_utils
+
+
+class StudentRatingCreateView(
+    LoginRequiredMixin,
+    # mixins_utils.StudentOnlyMixin,  # TODO: Disabled for debug purposes
+    CreateView,
+    mixins_utils.AutofillCreatedByRequestUser,
+):  # TODO: Debug view
+    """CreateView for Rating."""
+
+    model = StudentRating
+    form_class = StudentRatingForm
+    template_name = "rating/rating_createview.html"
