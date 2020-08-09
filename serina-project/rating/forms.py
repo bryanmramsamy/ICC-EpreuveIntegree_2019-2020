584d657a621387ff36f6a4e5d9930a15b5755984
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-08-09 20:35:34 +0200

    Hotfix: Add missing 'module' field to StudentRatingForm

--- serina-project/rating/forms.py
+++ serina-project/rating/forms.py
@@ -1,9 +1,14 @@
 from .models import StudentRating
+from management.models import Module
 from registration.utils import mixins as mixins_utils
 
 
 class StudentRatingForm(mixins_utils.HideCreatedByFieldFormMixin):
-    
+    module = mixins_utils.VerboseDegreeModuleChoiceField(
+        queryset=Module.objects.all(),
+        empty_label=None,
+    )
+
     class Meta(mixins_utils.HideCreatedByFieldFormMixin.Meta):
         model = StudentRating
-        fields = ("created_by", "rate", "comment",)
+        fields = ("created_by", "module", "rate", "comment",)
