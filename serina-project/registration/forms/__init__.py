083e0bd449392bfc641180bc92155cb73a9068cd
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-08-09 17:07:50 +0200

    Add SumbitFinalScore form in registration.forms.registration_actions

--- serina-project/registration/forms/__init__.py
+++ serina-project/registration/forms/__init__.py
@@ -1,3 +1,4 @@
 from .authentication import *
 from .profile import *
+from .registration_actions import *
 from .registration_report import *
---
+++ serina-project/registration/forms/registration_actions.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.utils.translation import ugettext as _
+
+
+class SubmitFinalScore(forms.Form):
+    """Form to submit a final score to a ModuleRegistrationReport instance."""
+
+    score = forms.DecimalField(
+        min_value=0,
+        max_value=100,
+        decimal_places=2,
+        label=_("Student's module final score"),
+    )
