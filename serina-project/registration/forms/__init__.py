3fbaee0dae0d138236e6f0b8d0c19d40516df1c9
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-08-10 20:36:38 +0200

    Debug: Add payment form

--- serina-project/registration/forms/__init__.py
+++ serina-project/registration/forms/__init__.py
@@ -1,4 +1,5 @@
 from .authentication import *
+from .payment import *
 from .profile import *
 from .registration_actions import *
 from .registration_report import *
---
+++ serina-project/registration/forms/payment.py
@@ -0,0 +1,8 @@
+from django import forms
+
+from ..models import ModuleRegistrationReport
+
+class PaymentForm(forms.Form):
+    """PaymentForm definition."""
+
+    module_rr = forms.ModelChoiceField(queryset=ModuleRegistrationReport.objects.all(), empty_label=None)
