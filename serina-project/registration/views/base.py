1a9b910161df4af8bdf235ed8215435ef4918880
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-08-14 03:30:32 +0200

    Add 3 random modules

--- serina-project/registration/views/base.py
+++ serina-project/registration/views/base.py
@@ -2,20 +2,21 @@ import random
 
 from django.shortcuts import render
 
-from management.models import Degree
+from management.models import Degree, Module
 
 
 def home(request):
     """Homepage render.
 
-    Add 3 random Degree instances to the context."""
+    Add 3 random Degree and Module instances to the context."""
 
     degrees = Degree.objects.order_by("?")[:3]
+    modules = Module.objects.order_by("?")[:3]
 
     return render(
         request,
         "registration/general/home.html",
-        {"degrees": degrees},
+        locals(),
     )
 
 
