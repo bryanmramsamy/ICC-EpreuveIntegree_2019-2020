0c182f70867310c380fd0fa2a4e4d4377f13f454
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-08-22 18:41:35 +0200

    Add 'module_already_validated_by_user' and 'all_prerequisites_validated_by_user' template filters

--- serina-project/registration/templatetags/registration_extra.py
+++ serina-project/registration/templatetags/registration_extra.py
@@ -1,7 +1,7 @@
 from django import template
 from django.contrib.auth.models import Group
 
-from ..utils import groups as groups_utils
+from ..utils import groups as groups_utils, registration as registration_utils
 
 
 register = template.Library()
@@ -20,6 +20,23 @@ def is_manager_or_administrator(user):
 
     return groups_utils.is_manager_or_administrator(user)
 
+
+@register.filter
+def module_already_validated_by_user(user, module):
+    """Filter that checks if the user has already vaidated the given module."""
+
+    return registration_utils.module_already_validated_by_user(user, module)
+
+
+@register.filter
+def all_prerequisites_validated_by_user(user, module):
+    """Filter that checks if the user has already vaidated the prerequisites
+    for the given module."""
+
+    return registration_utils.all_prerequisites_validated_by_user(user, module)
+
+
+# {{ request.user|module_already_validated_by_user:module }}
 
 # @register.filter(takes_context=True)
 # def user_is_student(context):
