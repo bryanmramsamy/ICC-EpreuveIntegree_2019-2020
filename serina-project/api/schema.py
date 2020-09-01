64b6924470570854e8141fdaea561ceae6b1acac
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-09-01 23:43:06 +0200

    Add module_levels, degrees, degree_categories and classrooms list as api queries

--- serina-project/api/schema.py
+++ serina-project/api/schema.py
@@ -9,7 +9,7 @@ from registration.models import *
 from registration.utils import management as management_utils
 
 
-# Graphene object declaration
+# Graphene types declaration
 
 class ModuleType(DjangoObjectType):
     """Django Object Type definition for Module."""
@@ -18,6 +18,33 @@ class ModuleType(DjangoObjectType):
         """Meta definition for ModuleType."""
 
         model = Module
+
+
+class ModuleLevelType(DjangoObjectType):
+    """Django Object Type definition for ModuleLevel."""
+
+    class Meta:
+        """Meta definition for ModuleLevelType."""
+
+        model = ModuleLevel
+
+
+class DegreeType(DjangoObjectType):
+    """Django Object Type definition for Degree."""
+
+    class Meta:
+        """Meta definition for DegreeType."""
+
+        model = Degree
+
+
+class DegreeCategoryType(DjangoObjectType):
+    """Django Object Type definition for DegreeCategory."""
+
+    class Meta:
+        """Meta definition for DegreeCategoryType."""
+
+        model = DegreeCategory
 
 
 class CourseType(DjangoObjectType):
@@ -31,13 +58,22 @@ class CourseType(DjangoObjectType):
         model = Course
 
 
+class ClassroomType(DjangoObjectType):
+    """Django Object Type definition for Classroom."""
+
+    class Meta:
+        """Meta definition for ClassroomType."""
+
+        model = Classroom
+
+
 class ManagementStatsType(graphene.ObjectType):
     """Graphene value declarations for management statistic data."""
 
     total_modules = graphene.Int()
     total_module_levels = graphene.Int()
     total_degrees = graphene.Int()
-    total_degrees_categories = graphene.Int()
+    total_degree_categories = graphene.Int()
     total_courses = graphene.Int()
     total_active_courses = graphene.Int()
     total_classrooms = graphene.Int()
@@ -49,7 +85,11 @@ class Query(graphene.ObjectType):
     """Root queries definitions."""
 
     modules = graphene.List(ModuleType)
+    module_levels = graphene.List(ModuleLevelType)
+    degrees = graphene.List(DegreeType)
+    degree_categories = graphene.List(DegreeCategoryType)
     courses = graphene.List(CourseType)
+    classrooms = graphene.List(ClassroomType)
 
     management_statistics = graphene.Field(ManagementStatsType)
 
@@ -58,10 +98,30 @@ class Query(graphene.ObjectType):
 
         return Module.objects.all()
 
+    def resolve_module_levels(self, info):
+        """Resolver for 'module_levels' query"""
+
+        return ModuleLevel.objects.all()
+
+    def resolve_degrees(self, info):
+        """Resolver for 'degrees' query"""
+
+        return Degree.objects.all()
+
+    def resolve_degree_categories(self, info):
+        """Resolver for 'degree_categories' query"""
+
+        return DegreeCategory.objects.all()
+
     def resolve_courses(self, info):
         """Resolver for 'courses' query"""
 
         return Course.objects.all()
+
+    def resolve_classrooms(self, info):
+        """Resolver for 'classrooms' query"""
+
+        return Classroom.objects.all()
 
     def resolve_management_statistics(self, info):
         """Resolver for 'managementStatistics' query"""
@@ -70,7 +130,7 @@ class Query(graphene.ObjectType):
             total_modules=Module.objects.all().count(),
             total_module_levels=ModuleLevel.objects.all().count(),
             total_degrees=Degree.objects.all().count(),
-            total_degrees_categories=DegreeCategory.objects.all().count(),
+            total_degree_categories=DegreeCategory.objects.all().count(),
             total_courses=Course.objects.all().count(),
             total_active_courses=management_utils.count_active_courses(),
             total_classrooms=Classroom.objects.all().count()
