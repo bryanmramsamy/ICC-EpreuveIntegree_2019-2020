7ad4b10baa29fe505a74a6de123d0f5c8e21570b
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-09-01 22:41:35 +0200

    Add count_module query

--- serina-project/api/schema.py
+++ serina-project/api/schema.py
@@ -7,20 +7,38 @@ from rating.models import *
 from registration.models import *
 
 
+# Graphene object declaration
+
 class ModulesType(DjangoObjectType):
+    """Django Object Type definition for Module."""
+
     class Meta:
+        """Meta definition for ModulesType."""
+
         model = Module
 
 
+class CountModulesStatType(graphene.ObjectType):
+    count_modules = graphene.Int()
+
+
+# Root queries
+
 class Query(graphene.ObjectType):
+    """Root queries definitions."""
+
     modules = graphene.List(ModulesType)
-    nb_modules = graphene.Int()
+    count_modules = graphene.Field(CountModulesStatType)
 
     def resolve_modules(self, info):
         return Module.objects.all()
 
-    def resolve_nb_modules(self, info):
-        return Module.objects.all().count()  # FIXME: Incorrect syntax
+    def resolve_count_modules(self, info):
+        return CountModulesStatType(
+            count_modules=Module.objects.all().count()
+        )
 
+
+# Schema
 
 schema = graphene.Schema(query=Query)
