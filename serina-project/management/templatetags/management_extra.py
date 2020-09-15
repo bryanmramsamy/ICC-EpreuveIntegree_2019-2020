6bc8cae740f72c03ec0bcaed8e8ad40d3bd0c3db
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-09-15 18:52:51 +0200

    Add is_eligible_teacher and is_prerequisite templatetags for ModuleUpdate mutliple select fields template

---
+++ serina-project/management/templatetags/management_extra.py
@@ -0,0 +1,25 @@
+from django import template
+from django.contrib.auth.models import User
+
+from ..models import Module
+from registration.utils import management as management_utils
+
+register = template.Library()
+
+
+# Module select mutliple filters
+
+@register.filter
+def is_eligible_teacher(teacher, module):
+    """Template tags checking if the user is an eligible teacher of the given
+    module."""
+
+    return management_utils.is_eligible_teacher(teacher, module)
+
+
+@register.filter
+def is_prerequisite(potential_prerequisite, module):
+    """Template tags checking if the module is a prerequiste of another
+    module."""
+
+    return management_utils.is_prerequisite(potential_prerequisite, module)
--- serina-project/registration/utils/management.py
+++ serina-project/registration/utils/management.py
@@ -1,5 +1,7 @@
-from management.models import Course
+from management.models import Course, Module
 
+
+# Courses
 
 def filter_active_courses():
     """Filter active courses based on their status."""
@@ -17,3 +19,17 @@ def count_active_courses():
     """Count the amount of active courses."""
 
     return len(filter_active_courses())
+
+
+# ManyToMany relationship check
+
+def is_eligible_teacher(teacher, module):
+    """Check if the user is an eligible teacher of the given module."""
+
+    return teacher.teachable_modules.filter(pk=module.pk).exists()
+
+
+def is_prerequisite(potential_prerequisite, module):
+    """Check if a module is a prerequisite of the other given module."""
+
+    return potential_prerequisite.postrequisites.filter(pk=module.pk).exists()
