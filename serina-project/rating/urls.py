f6127e685e84ac3f4354cd95b9de8caccb862817
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-09-30 14:47:04 +0200

    Remove Rating List and Detailview

--- serina-project/rating/urls.py
+++ serina-project/rating/urls.py
@@ -5,16 +5,6 @@ from . import views
 
 urlpatterns = [
     url(
-        r"l/$",
-        views.StudentRatingListView.as_view(),
-        name="rating_listview"
-    ),
-    url(
-        r"r/(?P<pk>[0-9]+)/$",
-        views.StudentRatingDetailView.as_view(),
-        name="rating_detailview"
-    ),
-    url(
         r"c/(?P<type>[a-z]{6})/(?P<pk>[0-9]+)/$",
         views.StudentRatingCreateView.as_view(),
         name="rating_createview"
