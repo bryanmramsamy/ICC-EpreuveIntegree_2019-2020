fa5d1445ba8c0a52619e6846e7ae06d1e565abdc
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-10-21 02:25:04 +0200

    Create custom docker images on registery

--- serina-project/scripts/deployment.sh
+++ serina-project/scripts/deployment.sh
@@ -2,18 +2,14 @@
 
 # Deployment script
 
-
 # Go to projet root
-
-cd ICC-EpreuveIntegree_2019-2020
+cd ~/ICC-EpreuveIntegree_2019-2020
 
 
 # Pull source code from repository
-
 git pull
 
 
 # Rebuild and restart the production docker containers
-
 docker-compose -f docker-compose.prod.yml down
 docker-compose -f docker-compose.prod.yml up -d --build
